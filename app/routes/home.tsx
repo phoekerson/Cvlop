35d148b39eef4fc2c7319f83a2cec77fee0e8cd8
Caleb Mintoumba <mintoumbacaleb@gmail.com>
2025-07-21 03:34:36 +0200

    install all dependencies

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,15 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Cvlop" },
+    { name: "description", content: "Opensource CV analyzer powered by AI " },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <main>
+    
+        </main>;
 }
