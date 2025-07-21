b25ce9283be4e87ff583d7a75a7b260a576023ee
Caleb Mintoumba <mintoumbacaleb@gmail.com>
2025-07-21 04:10:02 +0200

    set the data for landing page resume

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,14 +1,17 @@
+import NavBar from "~/components/NavBar";
 import type { Route } from "./+types/home";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "CVlop" },
+    { name: "Analyze our resume", content: "Opensource web app to help student and others analyzing resume by AI" },
   ];
 }
 
 export default function Home() {
   return <main>
+
+    <NavBar/>
     <section className="main-section">
       <div className="page-heading">
         <h1>Track Your Resume Ratings</h1>
